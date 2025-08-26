3afae574581f723a87ddd8e0b84176c4ec5fdba0
riz2001 <mohammedrizwanps@gmail.com>
2025-08-26 20:46:39 +0530

    completed

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
+import os
+
+app = Flask(__name__, static_folder="../frontend/build")
+CORS(app)
+
+@app.route("/api/message")
+def message():
+    return jsonify({"message": "Hello from Flask!"})
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, "index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
